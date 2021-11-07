380ae5eb0d14546f7d08bd034136b21fe14430a9
experisoutsourcin119 <juan.tenorio.fr@viaexperis.pe>
2021-11-06 19:38:16 -0500

    US01: ajustes varios

--- src/core/routes/routing/privado-routing.js
+++ src/core/routes/routing/privado-routing.js
@@ -1,15 +1,11 @@
 import { Redirect, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-<<<<<<< HEAD:src/core/routes/routing/privates-routing.js
-export const PrivatesRouting = ({ isAuthenticated, component: Component, ...rest }) => {
-=======
 export const PrivadoRouting = ({
   isAuthenticated,
   component: Component,
   ...rest
 }) => {
->>>>>>> 1cd421cffe1243fb0ccabcc2faa952470668b854:src/core/routes/routing/privado-routing.js
   return (
     <Route {...rest} component={(props) =>
       isAuthenticated ? (<Component {...props} />) : (<Redirect to="/inicio-sesion" />)
@@ -17,11 +13,4 @@ export const PrivadoRouting = ({
   );
 };
 
-<<<<<<< HEAD:src/core/routes/routing/privates-routing.js
 PrivatesRouting.propTypes = { isAuthenticated: PropTypes.bool.isRequired, component: PropTypes.func.isRequired };
-=======
-PrivadoRouting.propTypes = {
-  isAuthenticated: PropTypes.bool.isRequired,
-  component: PropTypes.func.isRequired,
-};
->>>>>>> 1cd421cffe1243fb0ccabcc2faa952470668b854:src/core/routes/routing/privado-routing.js
