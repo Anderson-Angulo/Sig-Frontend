c017fcc71eece6d30bb3fe20752ef12cced9afeb
Nataly Rojas <natalyrojasm5@gmail.com>
2021-11-06 22:07:53 -0500

    US01: Instalación de Tailwind CSS

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
+import './index.css';
 import 'shared/styles/base/global.scss';
 
 import reportWebVitals from './reportWebVitals';
