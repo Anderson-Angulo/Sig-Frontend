982d254639a68bbe4ec0a2eee2181d81da7de2fe
experisoutsourcin119 <juan.tenorio.fr@viaexperis.pe>
2021-11-06 19:38:49 -0500

    US01- ajustes varios

--- src/core/routes/routing/publico-routing.js
+++ src/core/routes/routing/publico-routing.js
@@ -1,15 +1,11 @@
 import { Redirect, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-<<<<<<< HEAD:src/core/routes/routing/public-routing.js
-export const PublicRouting = ({ isAuthenticated, component: Component, ...rest }) => {
-=======
 export const PublicoRouting = ({
   isAuthenticated,
   component: Component,
   ...rest
 }) => {
->>>>>>> 1cd421cffe1243fb0ccabcc2faa952470668b854:src/core/routes/routing/publico-routing.js
   return (
     <Route {...rest} component={(props) =>
       !isAuthenticated ? <Component {...props} /> : <Redirect to="/rrhh" />
@@ -17,11 +13,4 @@ export const PublicoRouting = ({
   );
 };
 
-<<<<<<< HEAD:src/core/routes/routing/public-routing.js
 PublicRouting.propTypes = { isAuthenticated: PropTypes.bool.isRequired, component: PropTypes.func.isRequired };
-=======
-PublicoRouting.propTypes = {
-  isAuthenticated: PropTypes.bool.isRequired,
-  component: PropTypes.func.isRequired,
-};
->>>>>>> 1cd421cffe1243fb0ccabcc2faa952470668b854:src/core/routes/routing/publico-routing.js
