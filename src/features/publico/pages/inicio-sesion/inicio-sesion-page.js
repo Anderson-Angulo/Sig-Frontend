02ca662cce9ede89136b89568fb908df4dc92cb9
Nataly Rojas <natalyrojasm5@gmail.com>
2021-11-06 22:55:23 -0500

    US01: Modificación de inicio de sesión

--- src/features/publico/pages/inicio-sesion/inicio-sesion-page.js
+++ src/features/publico/pages/inicio-sesion/inicio-sesion-page.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import PublicoLayout from 'shared/components/publico-layout/publico-layout';
 import './inicio-sesion-page.scss';
 
@@ -6,58 +6,59 @@ import { InputText } from 'primereact/inputtext';
 import { Password } from 'primereact/password';
 import { Checkbox } from 'primereact/checkbox';
 import { Button } from 'primereact/button';
+import ModalRecuperarContrasenaComponent from 'features/publico/components/modal-recuperar-contrasena-component';
 
 const InicioSesion = () => {
   const [checked, setChecked] = useState(false);
 
   return (
     <PublicoLayout page="login">
-      <form className="form-custom">
-        <header className="header">
-          <img
-            src="/images/logos/main-dark-logo.png"
-            className="w-full logo"
-            alt="Calidar"
-            title="Calidar"
-          />
-        </header>
-        <div className="fields">
-          <span className="p-float-label p-input-icon-right field w-full">
-            <i className="pi pi-user" />
-            <InputText id="email" name="email" />
-            <label htmlFor="email">Usuario</label>
-          </span>
-
-          <span className="p-float-label field">
-            <Password
-              id="password"
-              name="password"
-              className="w-full"
-              toggleMask
+      <Fragment>
+        <form className="form-custom">
+          <header className="header">
+            <img
+              src="/images/logos/main-dark-logo.png"
+              className="w-full logo"
+              alt="Calidar"
+              title="Calidar"
             />
-            <label htmlFor="password">Contraseña</label>
-          </span>
-        </div>
+          </header>
+          <div className="fields">
+            <span className="p-float-label p-input-icon-right field w-full">
+              <i className="pi pi-user" />
+              <InputText id="email" name="email" />
+              <label htmlFor="email">Usuario</label>
+            </span>
 
-        <div className="actions">
-          <p className="link">Olvidé mi contraseña</p>
-          <div className="p-field-checkbox field field-checkbox mt-2 w-full">
-            <Checkbox
-              inputId="remember"
-              checked={checked}
-              onChange={(e) => setChecked(e.checked)}
-            />
-            <label htmlFor="remember" className="label-checkbox">
-              {' '}
-              Recuérdame
-            </label>
+            <span className="p-float-label field">
+              <Password
+                id="password"
+                name="password"
+                className="w-full"
+                toggleMask
+              />
+              <label htmlFor="password">Contraseña</label>
+            </span>
           </div>
-          <Button
-            label="Ingresar"
-            className="btn btn-primary mt-4" /* loading={loading2} onClick={onLoadingClick2} */
-          />
-        </div>
-      </form>
+
+          <div className="actions">
+            <p className="link">Olvidé mi contraseña</p>
+            <div className="p-field-checkbox field field-checkbox mt-2 w-full">
+              <Checkbox
+                inputId="remember"
+                checked={checked}
+                onChange={(e) => setChecked(e.checked)}
+              />
+              <label htmlFor="remember" className="label-checkbox">
+                {' '}
+                Recuérdame
+              </label>
+            </div>
+            <Button label="Ingresar" className="btn btn-primary mt-4" />
+          </div>
+        </form>
+        <ModalRecuperarContrasenaComponent />
+      </Fragment>
     </PublicoLayout>
   );
 };
