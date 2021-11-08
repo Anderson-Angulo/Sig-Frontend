7f6a98470711f35578b3aaabeefff24122f69574
Nataly Rojas <natalyrojasm5@gmail.com>
2021-11-07 22:16:36 -0500

    US01: Cambio de nombre de componente de privado-layout

--- src/features/app-routing.js
+++ src/features/app-routing.js
@@ -1,32 +1,30 @@
-import { BrowserRouter as Router, Switch, Route, useRouteMatch } from "react-router-dom";
-import PrivateLayout from 'shared/components/privado-layout/private-layout';
-import { PublicoRouting } from "./publico/publico-routing";
-import RrhhRouting from "./rrhh/rrhh-routing";
-import RequireAuth from "../core/routes/required-auth";
-import InicioPage from "./dashboard/pages/inicio/inicio.page";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useRouteMatch,
+} from 'react-router-dom';
+import PrivadoLayout from 'shared/components/privado-layout/privado-layout';
+import { PublicoRouting } from './publico/publico-routing';
+import RrhhRouting from './rrhh/rrhh-routing';
+import RequireAuth from '../core/routes/required-auth';
+import InicioPage from './dashboard/pages/inicio/inicio.page';
 
 export const AppRouting = () => {
   const loggedIn = false;
-  console.log("AppRouting");
+  console.log('AppRouting');
   return (
     <>
       <Router>
         <Switch>
-
           <Route path={`/publico`} component={PublicoRouting} />
 
           <RequireAuth>
-            <PrivateLayout>
+            <PrivadoLayout>
               <Route path={`/rrhh`} component={RrhhRouting} />
               <Route component={InicioPage} />
-            </PrivateLayout>
+            </PrivadoLayout>
           </RequireAuth>
-
-
-
-
-
-
         </Switch>
       </Router>
     </>
