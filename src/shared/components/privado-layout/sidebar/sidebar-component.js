6f9976f0d492f90dbfe5b24a28f7c39c4e179683
experisoutsourcin119 <juan.tenorio.fr@viaexperis.pe>
2021-11-14 14:11:28 -0500

    US01: Menu del administrador dinamico

--- src/shared/components/privado-layout/sidebar/sidebar-component.js
+++ src/shared/components/privado-layout/sidebar/sidebar-component.js
@@ -3,12 +3,14 @@ import { Menu } from 'primereact/menu';
 import './sidebar-component.scss';
 
 const SidebarComponent = () => {
+  const usuarioInformation = useSelector(state => state.authReducer.user);
 
   let items = [
     { label: 'Gestión de Usuarios', icon: 'pi pi-user-edit' },
     { label: 'Roles y Privilegios', icon: 'pi pi-users' },
     { label: 'Configuración de Sistema', icon: 'pi pi-cog' },
   ];
+  
   return (
     <div className="sidebar">
       <Menu model={items} className="shadow-md rounded-md" />
