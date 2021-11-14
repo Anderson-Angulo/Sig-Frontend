1542df2ab21a3537970eff1b3ccdfecba212f7ef
Nataly Rojas <natalyrojasm5@gmail.com>
2021-11-14 00:44:08 -0500

    US01: Maquetación de tabla de roles

---
+++ src/features/configuracion/pages/roles-privilegio/datos/roles.js
@@ -0,0 +1,80 @@
+const tablaRoles = {
+  header: [
+    {
+      titulo: 'Nombre',
+    },
+    {
+      titulo: 'N° usuarios',
+    },
+    {
+      titulo: 'N° permisos',
+    },
+    {
+      titulo: 'Fecha creación',
+    },
+  ],
+  body: [
+    {
+      rol: 'Administrador',
+      nroUsuario: '1',
+      nroPermiso: '2',
+      fechaCreacion: '07/12/2021 07:27:03',
+    },
+    {
+      rol: 'Administrador',
+      nroUsuario: '1',
+      nroPermiso: '2',
+      fechaCreacion: '07/12/2021 07:27:03',
+    },
+    {
+      rol: 'Administrador',
+      nroUsuario: '1',
+      nroPermiso: '2',
+      fechaCreacion: '07/12/2021 07:27:03',
+    },
+    {
+      rol: 'Administrador',
+      nroUsuario: '1',
+      nroPermiso: '2',
+      fechaCreacion: '07/12/2021 07:27:03',
+    },
+    {
+      rol: 'Administrador',
+      nroUsuario: '1',
+      nroPermiso: '2',
+      fechaCreacion: '07/12/2021 07:27:03',
+    },
+    {
+      rol: 'Administrador',
+      nroUsuario: '1',
+      nroPermiso: '2',
+      fechaCreacion: '07/12/2021 07:27:03',
+    },
+    {
+      rol: 'Administrador',
+      nroUsuario: '1',
+      nroPermiso: '2',
+      fechaCreacion: '07/12/2021 07:27:03',
+    },
+    {
+      rol: 'Administrador',
+      nroUsuario: '1',
+      nroPermiso: '2',
+      fechaCreacion: '07/12/2021 07:27:03',
+    },
+    {
+      rol: 'Administrador',
+      nroUsuario: '1',
+      nroPermiso: '2',
+      fechaCreacion: '07/12/2021 07:27:03',
+    },
+    {
+      role: 'Administrador',
+      nroUsuario: '1',
+      nroPermiso: '2',
+      fechaCreacion: '07/12/2021 07:27:03',
+    },
+  ],
+};
+
+export default tablaRoles;
