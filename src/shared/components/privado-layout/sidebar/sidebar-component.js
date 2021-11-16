606acf59501416ed6174e9009072eb7956f443b4
Nataly Rojas <natalyrojasm5@gmail.com>
2021-11-15 22:25:50 -0500

    US01: Corección cuando se da click en item padre y no tiene url

--- src/shared/components/privado-layout/sidebar/sidebar-component.js
+++ src/shared/components/privado-layout/sidebar/sidebar-component.js
@@ -6,17 +6,16 @@ import { useSelector } from 'react-redux';
 
 const SidebarComponent = () => {
   const history = useHistory();
-  const usuarioInformation = useSelector(state => state.authReducer.user);
+  const usuarioInformation = useSelector((state) => state.authReducer.user);
 
   const [menus, setMenus] = useState([]);
   const [selectedKey, setSelectedKey] = useState(null);
 
   useEffect(() => {
     let options = [];
-    usuarioInformation?.menuPrincipal?.forEach(menu => {
-
+    usuarioInformation?.menuPrincipal?.forEach((menu) => {
       let children = [];
-      menu?.subMenus?.forEach(subMenu => {
+      menu?.subMenus?.forEach((subMenu) => {
         children.push({
           key: subMenu.codigo,
           label: subMenu.descripcion,
@@ -29,11 +28,16 @@ const SidebarComponent = () => {
         label: menu.descripcion,
         url: menu.url,
         icon: menu.icono,
-        children: children
+        children: children,
       });
     });
-    setMenus(options);
 
+    options = options.filter((option) => {
+      if (!option.url) delete option.url;
+      return option;
+    });
+
+    setMenus(options);
   }, [usuarioInformation]);
 
   return (
