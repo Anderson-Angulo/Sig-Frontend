3ee53b7d9d5767b42f490348bed469e03bef1c81
experisoutsourcin119 <juan.tenorio.fr@viaexperis.pe>
2021-11-15 20:50:17 -0500

    US01: Sidebar dinamico de acuerdo al servicio

--- src/shared/components/privado-layout/sidebar/sidebar-component.js
+++ src/shared/components/privado-layout/sidebar/sidebar-component.js
@@ -1,58 +1,45 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import { Tree } from 'primereact/tree';
 import './sidebar-component.scss';
+import { useSelector } from 'react-redux';
 
 const SidebarComponent = () => {
   const history = useHistory();
-  //const usuarioInformation = useSelector(state => state.authReducer.user);
-  const [items, _] = useState([
-    {
-      key: '0',
-      label: 'Gestión de Usuarios',
-      url: '/configuracion/usuarios',
-      icon: 'pi pi-user-edit',
-      children: [
-        {
-          key: '0-0',
-          label: 'Sub-Item',
-          url: '/configuracion/usuarios',
-        },
-      ],
-    },
-    {
-      key: '1',
-      label: 'Roles y Privilegios',
-      url: '/configuracion/rol-privilegios',
-      icon: 'pi pi-users',
-      children: [
-        {
-          key: '1-0',
-          label: 'Sub-Item',
-          url: '/configuracion/rol-privilegios',
-        },
-      ],
-    },
-    {
-      key: '2',
-      label: 'Configuración de Sistema',
-      url: '/configuracion/sistema',
-      icon: 'pi pi-cog',
-      children: [
-        {
-          key: '2-0',
-          label: 'Sub-Item',
-          url: '/configuracion/sistema',
-        },
-      ],
-    },
-  ]);
+  const usuarioInformation = useSelector(state => state.authReducer.user);
+
+  const [menus, setMenus] = useState([]);
   const [selectedKey, setSelectedKey] = useState(null);
+
+  useEffect(() => {
+    let options = [];
+    usuarioInformation?.menuPrincipal?.forEach(menu => {
+
+      let children = [];
+      menu?.subMenus?.forEach(subMenu => {
+        children.push({
+          key: subMenu.codigo,
+          label: subMenu.descripcion,
+          url: subMenu.url,
+        });
+      });
+
+      options.push({
+        key: menu.codigo,
+        label: menu.descripcion,
+        url: menu.url,
+        icon: menu.icono,
+        children: children
+      });
+    });
+    setMenus(options);
+
+  }, [usuarioInformation]);
 
   return (
     <div className="sidebar">
       <Tree
-        value={items}
+        value={menus}
         className="shadow-md rounded-md"
         selectionMode="single"
         selectionKeys={selectedKey}
