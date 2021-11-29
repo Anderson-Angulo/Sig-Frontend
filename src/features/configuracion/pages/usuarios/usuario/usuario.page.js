1b3c55fd557be8e5b312ffb1a67f83961a14484f
Nataly Rojas <natalyrojasm5@gmail.com>
2021-11-29 00:29:34 -0500

    US01: Maquetación de nuevo usuario

--- src/features/configuracion/pages/usuarios/usuario/usuario.page.js
+++ src/features/configuracion/pages/usuarios/usuario/usuario.page.js
@@ -1,15 +1,26 @@
 import { Fragment } from 'react';
+import { ListBox } from 'primereact/listbox';
 import { useHistory } from 'react-router';
+import { Accordion, AccordionTab } from 'primereact/accordion';
+import { InputSwitch } from 'primereact/inputswitch';
 import { Panel } from 'primereact/panel';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
+
+import './usuario.page.scss';
 
 const UsuarioPage = () => {
   const history = useHistory();
 
   const isActive = false;
+
+  const sedesList = [
+    { label: 'Sede 1', value: 'SEDE1' },
+    { label: 'Sede 2', value: 'SEDE2' },
+    { label: 'Sede 3', value: 'SEDE3' },
+  ];
   return (
-    <Fragment>
+    <div className="bg-white p-10 mt-3 rounded-md shadow-md">
       <Panel header="DATOS GENERALES" toggleable>
         <form className="form-custom p-0 my-4 mx-4">
           <div className="flex justify-end mb-6">
@@ -43,11 +54,29 @@ const UsuarioPage = () => {
           </div>
         </form>
       </Panel>
-      <Panel header="EMPRESAS" toggleable className="mt-3">
-        <h1>Empresas</h1>
+      <Panel header="ROLES" toggleable className="mt-3">
+        <div className="user-roles py-2 px-5">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num) => (
+            <div className="flex items-center gap-6">
+              <InputSwitch key={num} />
+              <p>ROL {num} </p>
+            </div>
+          ))}
+        </div>
       </Panel>
-      <Panel header="SEDES" toggleable className="mt-3">
-        <h1>Sedes</h1>
+      <Panel header="EMPRESAS" toggleable className="mt-3">
+        <Accordion>
+          <AccordionTab
+            header={
+              <Fragment>
+                <i className="pi pi-building"></i> EMPRESA 1
+              </Fragment>
+            }
+          >
+            <h5 className="font-bold mb-3">SEDE</h5>
+            <ListBox multiple options={sedesList} />
+          </AccordionTab>
+        </Accordion>
       </Panel>
 
       <div className="flex gap-4 mt-3">
@@ -65,7 +94,7 @@ const UsuarioPage = () => {
           onClick={() => history.goBack()}
         />
       </div>
-    </Fragment>
+    </div>
   );
 };
 
