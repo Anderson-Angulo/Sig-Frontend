ed860a2e8dca23408fddd46126f76f1b13dd90e1
Nataly Rojas <natalyrojasm5@gmail.com>
2021-12-08 12:47:12 -0500

    US01: Modificación de logo header

--- src/shared/components/logo/logo-component.js
+++ src/shared/components/logo/logo-component.js
@@ -1,16 +1,14 @@
-import { useSelector } from 'react-redux';
 import './logo-component.scss';
 const LogoComponent = () => {
-  const isOpen = useSelector((state) => state.toggleSidebarReducer.isOpen);
   return (
     <div className="logo-main flex items-center gap-2">
       <img
-        src="/images/logos/logo-icon.png"
+        src="/images/logos/main-logo.png"
         className=" logo-icon"
         alt="Calidar"
         title="Calidar"
       />
-      {!isOpen && <h1 className="name-company">Calidar</h1>}
+      {/* <h1 className="name-company">Calidar</h1> */}
     </div>
   );
 };
