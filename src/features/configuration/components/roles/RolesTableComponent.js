83bf5bf6f31e07b1a011adc988e15b28eef77bc5
Nataly Rojas <natalyrojasm5@gmail.com>
2021-12-17 00:31:25 -0500

    US01: Acciones de mostrar subtabla y opciones

--- src/features/configuration/components/roles/RolesTableComponent.js
+++ src/features/configuration/components/roles/RolesTableComponent.js
@@ -1,35 +1,22 @@
-import { useSelector } from 'react-redux';
-import { Button } from 'primereact/button';
 import { Fragment, useState } from 'react';
+import { useSelector } from 'react-redux';
+
 import TableHeader from 'shared/components/tables/TableHeader';
 import { RolesTableData } from 'features/configuration/data/roles/RolesTableData';
 import TableItem from 'shared/components/tables/TableItem';
 import TablePagination from 'shared/components/tables/TablePagination';
 const RolesTableComponent = () => {
   const roles = useSelector((state) => state.roleReducer.roles);
-  const { RoleTableHeader } = RolesTableData;
-  const [currentUser, setCurrentUser] = useState('');
-  const [showInfoUserID, setShowInfoUserID] = useState('');
+  const { RoleTableHeader, RoleSubTableHeader } = RolesTableData;
+  const [options, setOptions] = useState({
+    currentID: '',
+    showOptions: false,
+    showSubTable: false,
+  });
 
-  /* const tableSecondaryHeader = [
-    'Usuario',
-    'Apellidos',
-    'Nombres',
-    'Fecha de creación',
-  ]; */
-  /*   const user = [
-    {
-      rol: 'ADMINISTRADOR',
-      nro_usuarios: 50,
-      nro_permisos: 5,
-      sistema: 'SI',
-      fecha_creacion: '04/11/21',
-    },
-  ];
-
-  const tableRoles = Array.from(Array(10)).map((_, index) => {
-    return { ...user[0], id: index };
-  }); */
+  const showOption = (fields) => {
+    setOptions({ ...options, ...fields });
+  };
 
   return (
     <Fragment>
@@ -42,7 +29,16 @@ const RolesTableComponent = () => {
           currentCols={RoleTableHeader.length}
           isLoading={roles.loading}
           listItem={roles.data}
+          showOption={showOption}
           tableName="table-roles"
+          currentOptions={options}
+          SubTableHeader={() => (
+            <TableHeader
+              listHeader={RoleSubTableHeader}
+              currentHeader=""
+              showIcon={false}
+            />
+          )}
         />
         {roles.data.length > 0 && (
           <TablePagination
@@ -50,79 +46,6 @@ const RolesTableComponent = () => {
             pageSize={roles.pagination.pageSize}
           />
         )}
-
-        {/* <div className="table-body relative">
-          {tableRoles.length > 0 && (
-            <Fragment>
-              {tableRoles.map((item, index) => (
-                <Fragment key={index}>
-                  <div
-                    className={`table-item text-center ${
-                      currentUser === item.id ? 'selected' : ''
-                    }`}
-                  >
-                    <p>
-                      {showInfoUserID !== item.id ? (
-                        <i
-                          className="pi pi-angle-down cursor-pointer"
-                          onClick={() => setShowInfoUserID(item.id)}
-                        ></i>
-                      ) : (
-                        <i
-                          className="pi pi-angle-right cursor-pointer"
-                          onClick={() => setShowInfoUserID('')}
-                        ></i>
-                      )}
-                    </p>
-                    <p>{item.rol}</p>
-                    <p>{item.nro_usuarios}</p>
-                    <p>{item.nro_permisos}</p>
-                    <p>{item.sistema}</p>
-                    <p>{item.fecha_creacion}</p>
-                    <i
-                      className="pi pi-ellipsis-v cursor-pointer"
-                      onClick={() => setCurrentUser(item.id)}
-                    ></i>
-                                        {currentUser === item.id && (
-                      <div className="table-item-actions-selected absolute w-full z-50 shadow-md rounded-md">
-                        <i
-                          className="pi pi-times absolute cursor-pointer rounded-full flex items-center"
-                          onClick={() => setCurrentUser('')}
-                        ></i>
-                        <div className="item-selected-actions flex flex-col">
-                          <button>Editar</button>
-                        </div>
-                      </div>
-                    )}
-                  </div>
-                  {showInfoUserID === item.id && (
-                    <div className="table-secondary p-6 bg-white">
-                      <div className="table-header">
-                        {tableSecondaryHeader.map((header) => (
-                          <div
-                            className="header-title flex items-center justify-center gap-x-3"
-                            key={index}
-                          >
-                            <h3 className="text uppercase">{header}</h3>
-                          </div>
-                        ))}
-                      </div>
-                      <div className="table-item">
-                        <p>nataly@gmail.com</p>
-                        <p>Rojas</p>
-                        <p>Brigitte Nataly</p>
-                        <p>20/02/2021</p>
-                      </div>
-                    </div>
-                  )}
-                </Fragment>
-              ))}
-            </Fragment>
-          )}
-        </div> */}
-        {/* {tableRoles.length > 0 && (
-          
-        )} */}
       </div>
     </Fragment>
   );
