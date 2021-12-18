708ffe5711138062871f986c177ddc467bfaa018
Nataly Rojas <natalyrojasm5@gmail.com>
2021-12-18 18:41:33 -0500

    US01: Eliminacion de accion de busqueda de roles del lado de frontend

--- src/features/configuration/store/actions/RolesAction.js
+++ src/features/configuration/store/actions/RolesAction.js
@@ -44,22 +44,6 @@ const getRoles = (fields) => {
   };
 };
 
-const searchRole = (value) => {
-  return (dispatch, getState) => {
-    let { pagination, data, currentData } = getState().roleReducer.roles;
-    const loading = false;
-
-    if (value.length > 2) {
-      data = currentData.filter(({ roleName }) =>
-        roleName.toLowerCase().includes(value.trim().toLowerCase())
-      );
-      dispatch(setRoles({ loading, data, pagination, currentData }));
-      return;
-    }
-    dispatch(setRoles({ loading, data: currentData, currentData, pagination }));
-  };
-};
-
 const setRolesOptions = (payload) => {
   /* payload: { loading, options } */
   return (dispatch) => {
@@ -159,7 +143,6 @@ const removeFilterValues = (field) => {
 
 export const RolesAction = {
   getRoles,
-  searchRole,
   toggleModalFilters,
   getRolesOptions,
   getUserByRoleId,
