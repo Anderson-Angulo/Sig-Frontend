e39b36653151b9df660fc52a6b6e1d6c57af5fb3
Nataly Rojas <natalyrojasm5@gmail.com>
2021-12-23 23:19:18 -0500

    US01: Paginacion de tabla de usuarios

--- src/features/configuration/components/users/UsersTableComponent.js
+++ src/features/configuration/components/users/UsersTableComponent.js
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 import TableHeader from 'shared/components/tables/TableHeader';
 import TableItem from 'shared/components/tables/TableItem';
 import TableActions from 'shared/components/tables/TableActions';
+import TablePagination from 'shared/components/tables/TablePagination';
 
 const UsersTableComponent = () => {
   const { UsersTableHeader, UsersTableActions } = UsersTableData;
@@ -19,10 +20,6 @@ const UsersTableComponent = () => {
   };
 
   const currentAction = (action) => {
-    /*  if (action === 'role-edit')
-        history.push(
-          `/configuracion/rol-privilegios/editar/${options.currentID}`
-        ); */
     console.log('action ', action);
   };
 
@@ -34,7 +31,6 @@ const UsersTableComponent = () => {
           colMain="email"
           currentHeader={users.currentCol}
         />
-
         <TableItem
           currentCols={UsersTableHeader.length}
           isLoading={users.loading}
@@ -50,92 +46,13 @@ const UsersTableComponent = () => {
             />
           )}
         />
-        {/* <div className="table-body relative">
-          {tableUsers.length > 0 && (
-            <Fragment>
-              {tableUsers.map((item, index) => (
-                <Fragment key={index}>
-                  <div
-                    className={`table-item text-center ${
-                      currentUser === item.id ? 'selected' : ''
-                    }`}
-                  >
-                    <p>{item.usuario}</p>
-                    <p>{item.nombres}</p>
-                    <p>{item.apellidos}</p>
-                    <p>{item.fecha_creacion}</p>
-                    <p>{item.ultimo_acceso}</p>
-                    <p>{item.ultimo_acceso}</p>
-                    <i
-                      className="pi pi-ellipsis-v cursor-pointer"
-                      onClick={() => setCurrentUser(item.id)}
-                    ></i>
 
-                    {currentUser === item.id && (
-                      <div className="table-item-actions-selected absolute w-full z-50 shadow-md rounded-md">
-                        <i
-                          className="pi pi-times absolute cursor-pointer rounded-full flex items-center"
-                          onClick={() => setCurrentUser('')}
-                        ></i>
-                        <div className="item-selected-actions flex flex-col">
-                          <button>Editar</button>
-                          <button>Eliminar</button>
-                          <button>Resetear contraseña</button>
-                        </div>
-                      </div>
-                    )}
-                  </div>
-                </Fragment>
-              ))}
-            </Fragment>
-          )}
-          {tableUsers.length <= 0 && (
-            <div className="table-body-empty">
-              <i className="pi pi-inbox"></i>
-              <h3>No hay datos</h3>
-            </div>
-          )}
-        </div>
-
-        {tableUsers.length > 0 && (
-          <div className="table-pagination flex items-center justify-end gap-4 p-5">
-            <div className="pagination-pages">
-              <span
-                style={{
-                  color: '#607D8B',
-                  fontWeight: 'bold',
-                }}
-              >
-                Pág. 1 de 2
-              </span>
-            </div>
-            <div className="pagination-rows">
-              <select>
-                <option>Mostrar 2 filas</option>
-              </select>
-            </div>
-            <div className="pagination-actions flex justify-end gap-2">
-              <Button
-                label="<<"
-                className="p-button-secondary p-button-outlined"
-              />
-
-              <Button
-                label="Anterior"
-                className="p-button-secondary p-button-outlined"
-              />
-              <Button
-                label="Siguiente"
-                className="p-button-secondary p-button-outlined"
-              />
-
-              <Button
-                label=">>"
-                className="p-button-secondary p-button-outlined"
-              />
-            </div>
-          </div>
-        )} */}
+        {users.data.length > 0 && users.pagination.currentPage > 1 && (
+          <TablePagination
+            currentPage={users.pagination.currentPage}
+            pageSize={users.pagination.pageCount}
+          />
+        )}
       </div>
     </Fragment>
   );
