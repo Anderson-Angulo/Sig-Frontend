f342d46dc301d66ccd482dc7bb057bbd73454fbf
Nataly Rojas <natalyrojasm5@gmail.com>
2021-12-31 16:42:07 -0500

    US01: Cambio visual de editar y crear rol

--- src/features/configuration/pages/roles-privileges/RolPrivilegePage.js
+++ src/features/configuration/pages/roles-privileges/RolPrivilegePage.js
@@ -9,6 +9,8 @@ import Message from 'shared/components/messages/Message';
 import './RolePrivilege.scss';
 
 import useRole from 'features/configuration/hooks/roles/useRole';
+import { Panel } from 'primereact/panel';
+import { InputSwitch } from 'primereact/inputswitch';
 
 const RolPrivilegioPage = ({ title = 'NUEVO ROL' }) => {
   const {
@@ -46,79 +48,76 @@ const RolPrivilegioPage = ({ title = 'NUEVO ROL' }) => {
   };
 
   return (
-    <div className="bg-white p-10  rounded-md shadow-md">
+    <div className="bg-white p-8  rounded-md shadow-md">
       <form className="form-custom p-0" onSubmit={handleSubmit}>
-        {showField() && (
-          <div className="mb-6 w-2/5">
-            <span className="p-float-label w-full">
-              <InputText
-                type="text"
-                id="rol_name"
-                value={roles.name}
-                onChange={handleChange}
-                className={errors.showInRole ? 'p-invalid' : ''}
-              />
-              <label htmlFor="rol_name">Nombre del rol*</label>
-            </span>
+        <Panel header={title} toggleable>
+          {showField() && (
+            <div className="mb-6 w-2/5 mt-5">
+              <span className="p-float-label w-full">
+                <InputText
+                  type="text"
+                  id="rol_name"
+                  value={roles.name}
+                  onChange={handleChange}
+                  className={errors.showInRole ? 'p-invalid' : ''}
+                />
+                <label htmlFor="rol_name">Nombre del rol*</label>
+              </span>
+            </div>
+          )}
+          <div className="grid-roles scroll mt-8">
+            {showSkeleton() &&
+              [1, 2, 3, 4, 5].map((i) => <OptionSkeleton key={i} />)}
+            {showRoles() &&
+              options.map((option, index) => {
+                return (
+                  <div className="rol " key={index}>
+                    <div className="rol-title">
+                      <h2 className="font-semibold">{option.name}</h2>
+                    </div>
+                    <div className="flex gap-3"></div>
+                    <div className="rol-list mt-2">
+                      {option.actions.map((action, i) => (
+                        <div className="mb-4 flex gap-3" key={i}>
+                          <InputSwitch
+                            inputId={action.name}
+                            id={action.id}
+                            checked={isChecked(action)}
+                            onChange={handleCheckbox}
+                            className={errors.showInActions ? 'p-invalid' : ''}
+                          />
+                          <p>{action.name}</p>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                );
+              })}
           </div>
-        )}
-        <div className="grid-roles scroll">
-          {showSkeleton() &&
-            [1, 2, 3, 4, 5].map((i) => <OptionSkeleton key={i} />)}
-          {showRoles() &&
-            options.map((option, index) => {
-              return (
-                <div className="rol mt-4" key={index}>
-                  <div className="rol-title">
-                    <h2 className="font-semibold">{option.name}</h2>
-                  </div>
-                  <div className="rol-list mt-2">
-                    {option.actions.map((action, i) => (
-                      <div className="mb-2" key={i}>
-                        <Checkbox
-                          inputId={action.name}
-                          id={action.id}
-                          value={action.name}
-                          checked={isChecked(action)}
-                          onChange={handleCheckbox}
-                          className={errors.showInActions ? 'p-invalid' : ''}
-                        ></Checkbox>
-                        <label
-                          htmlFor={action.name}
-                          className="p-checkbox-label ml-3"
-                        >
-                          {action.name}
-                        </label>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              );
-            })}
-        </div>
-        {(errors.showInRole ||
-          errors.showInActions ||
-          errors.list.length > 0) && (
-          <Message messages={errors.list} status="error" />
-        )}
+          {(errors.showInRole ||
+            errors.showInActions ||
+            errors.list.length > 0) && (
+            <Message messages={errors.list} status="error" />
+          )}
 
-        <div className="flex gap-3 items-center justify-end mt-3">
-          <Button
-            icon="pi pi-times"
-            type="button"
-            label="Cancelar"
-            className="btn btn-secondary mt-4"
-            onClick={() => setVisible(true)}
-            disabled={status === ''}
-          />
-          <Button
-            icon="pi pi-save"
-            type="submit"
-            label="Guardar"
-            loading={status === ''}
-            className="btn btn-primary mt-4"
-          />
-        </div>
+          <div className="flex gap-3 items-center justify-end mt-3">
+            <Button
+              icon="pi pi-times"
+              type="button"
+              label="Cancelar"
+              className="btn btn-secondary mt-4"
+              onClick={() => setVisible(true)}
+              disabled={status === ''}
+            />
+            <Button
+              icon="pi pi-save"
+              type="submit"
+              label="Guardar"
+              loading={status === ''}
+              className="btn btn-primary mt-4"
+            />
+          </div>
+        </Panel>
       </form>
       <ConfirmDialog
         visible={visible}
