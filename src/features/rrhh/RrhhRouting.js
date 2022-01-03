2b900b44fa6c68b05c1b06faef88b97c01220d46
Rodrigo Matteo <rodrigo.matteo@gmail.com>
2022-01-03 17:47:06 -0500

    Rename WorkstationPage to PositionPage

--- src/features/rrhh/RrhhRouting.js
+++ src/features/rrhh/RrhhRouting.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route, useRouteMatch } from 'react-router-dom';
 import PersonPage from './pages/people/PersonPage';
 import PeoplePage from './pages/people/PeoplePage';
-import WorkstationPage from './pages/workstation/WorkstationPage';
+import PositionPage from './pages/positions/PositionPage';
 
 export default function RrhhRouting() {
   const { path } = useRouteMatch();
@@ -13,8 +13,8 @@ export default function RrhhRouting() {
           <Route exact path={`${path}/personas`} component={PeoplePage} ren />
           <Route path={`${path}/personas/:id`} component={PersonPage} />
         </Route>
-        <Route path={`${path}/puestos-trabajo`}>
-          <Route path={`${path}/puestos-trabajo`} component={WorkstationPage} />
+        <Route path={`${path}/cargos`}>
+          <Route path={`${path}/cargos`} component={PositionPage} />
         </Route>
       </Switch>
     </>
---
+++ src/features/rrhh/pages/positions/PositionPage.js
@@ -0,0 +1,9 @@
+// import PropTypes from 'prop-types';
+
+const PositionPage = (props) => {
+  return <div>Cargos</div>;
+};
+
+// PuestoTrabajos.propTypes = {};
+
+export default PositionPage;
