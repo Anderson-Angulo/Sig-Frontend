cd14f0375e80b385f9bbcdb996139aeb38c902e5
Rodrigo Matteo <rodrigo.matteo@gmail.com>
2022-01-04 00:21:46 -0500

    US03: New Position (cargo) record

--- src/features/rrhh/RrhhRouting.js
+++ src/features/rrhh/RrhhRouting.js
@@ -3,6 +3,7 @@ import { Switch, Route, useRouteMatch } from 'react-router-dom';
 import PersonPage from './pages/people/PersonPage';
 import PeoplePage from './pages/people/PeoplePage';
 import PositionsPage from './pages/positions/PositionsPage';
+import PositionPage from './pages/positions/PositionPage';
 
 export default function RrhhRouting() {
   const { path } = useRouteMatch();
@@ -13,8 +14,10 @@ export default function RrhhRouting() {
           <Route exact path={`${path}/personas`} component={PeoplePage} ren />
           <Route path={`${path}/personas/:id`} component={PersonPage} />
         </Route>
-        <Route path={`${path}/cargos`}>
-          <Route path={`${path}/cargos`} component={PositionsPage} />
+        <Route path={`${path}/cargos`} component={PositionsPage}>
+          <Route exact path={`${path}/cargos`} component={PositionsPage} />
+          <Route exact path={`${path}/cargos/nuevo`} component={() => <PositionPage title="NUEVO CARGO" />}
+        />
         </Route>
       </Switch>
     </>
--- src/features/rrhh/pages/positions/PositionPage.js
+++ src/features/rrhh/pages/positions/PositionPage.js
@@ -1,9 +1,66 @@
-// import PropTypes from 'prop-types';
+import { Panel } from 'primereact/panel';
+import { InputText } from 'primereact/inputtext';
+import { InputNumber } from 'primereact/inputnumber';
+import { Button } from 'primereact/button';
 
-const PositionPage = (props) => {
-  return <div>Cargos</div>;
+const PositionPage = ({ title = 'NUEVO CARGO' }) => {
+  return (
+    <div className="bg-white p-8  rounded-md shadow-md">
+      <form className="form-custom p-0" onSubmit={null}>
+        <Panel header={title} toggleable>
+          <div className="mb-6 w-2/5 mt-5">
+            <span className="p-float-label w-full">
+              <InputText
+                type="text"
+                id="position_name"
+                value={null}
+                onChange={null}
+                className=''
+              />
+              <label htmlFor="rol_name">Nombre del cargo*</label>
+            </span>
+          </div>
+          <div className="mb-6 w-4/5 mt-5">
+            <span className="p-float-label w-full">
+              <InputText
+                type="text"
+                id="position_name"
+                value={null}
+                onChange={null}
+                className=''
+              />
+              <label htmlFor="position_name">Descripción*</label>
+            </span>
+          </div>
+          <div className="mb-6 w-4/5 mt-5">
+            <span className="p-float-label w-full">
+              <InputNumber 
+                id="employees_quantity" 
+                value={null} 
+                onValueChange={null} 
+                className=''/>
+              <label htmlFor="employees_quantity">Nro. Empleados*</label>
+            </span>
+          </div>
+          <div className="flex justify-end gap-4 mt-3">
+            <Button
+              icon="pi pi-times"
+              type="button"
+              label="Cancelar"
+              onClick={null}
+              className="btn btn-secondary mt-4"
+            />
+            <Button
+              icon="pi pi-save"
+              type="submit"
+              label="Guardar"
+              className="btn btn-primary mt-4"
+            />
+          </div>
+        </Panel>
+      </form>
+    </div>
+  )
 };
-
-// PuestoTrabajos.propTypes = {};
 
 export default PositionPage;
